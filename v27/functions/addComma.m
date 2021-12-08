d2d875a7071cbff40ae1fe35c14e87bb3d46973c
Oytun Gunes <71605451+oytungunes@users.noreply.github.com>
2021-12-08 20:29:36 +0300

    Delete v27 directory

--- v27/functions/addComma.m
+++
@@ -1,4 +0,0 @@
-function numOut = addComma(numIn)
-   jf=java.text.DecimalFormat; % comma for thousands, three decimal places
-   numOut= char(jf.format(numIn)); % omit "char" if you want a string out
-end
